9308b844785842b44c20e0da4d01726afdfe4d6a
baoTran <135978806+baosetsuna123@users.noreply.github.com>
2024-06-04 22:35:18 +0700

    Revert "create API Create and update Promotion"
    This reverts commit fc643b11b55dfedaa9ef9805ac5e4390a0d1955c.

--- JewelrySalesSystem_NoName_BE/JSS_Services/Interface/IPromotionService.cs
+++ JewelrySalesSystem_NoName_BE/JSS_Services/Interface/IPromotionService.cs
@@ -5,8 +5,6 @@ namespace JSS_Services.Interface
     public interface IPromotionService
     {
         Task<IEnumerable<Promotion>> GetAllPromotionsAsync();
-        Task<Promotion> CreatePromotionAsync(Promotion newData);
         Task<Promotion> GetPromotionByIdAsync(Guid id);
-        Task<Promotion> UpdatePromotionAsync(Guid id, Promotion updatedData);
     }
 }
