9299a0d0913dc509019e51c1e31870aa96dfb6ee
ngandolh <nganttk002@gmail.com>
2024-06-08 09:40:27 +0700

    update: Create order v2 - Choose promotion, display total price

--- JewelrySalesSystem_NoName_FE/Pages/Staff/Orders/AddOrder.cshtml.cs
+++ JewelrySalesSystem_NoName_FE/Pages/Staff/Orders/AddOrder.cshtml.cs
@@ -1,3 +1,4 @@
+using JewelrySalesSystem_NoName_FE.DTOs.Account;
 using JewelrySalesSystem_NoName_FE.DTOs.Product;
 using JewelrySalesSystem_NoName_FE.Ultils;
 using Microsoft.AspNetCore.Mvc;
@@ -9,17 +10,26 @@ namespace JewelrySalesSystem_NoName_FE.Pages.Staff.Orders
     public class AddOrderModel : PageModel
     {
         private readonly HttpClient _httpClient;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public AddOrderModel(HttpClient httpClient)
+        public AddOrderModel(IHttpClientFactory httpClientFactory, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
         {
-            _httpClient = httpClient;
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+            _httpClient = _httpClientFactory.CreateClient();
         }
+
+        public IList<Promotion> listPromotions { get; set; } = new List<Promotion>();
 
         public async Task<JsonResult> OnGetProductAsync(string productCode)
         {
             var apiUrl = $"{ApiPath.ProductCodeGetListPromoton}?productCode={productCode}";
             var response = await _httpClient.GetStringAsync(apiUrl);
             var product = JsonConvert.DeserializeObject<ProductResponse>(response);
+            listPromotions = product.Promotions.ToList();
             return new JsonResult(product);
         }
     }
