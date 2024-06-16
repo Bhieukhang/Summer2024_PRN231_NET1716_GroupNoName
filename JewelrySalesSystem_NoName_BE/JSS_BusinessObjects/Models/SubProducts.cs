623aeadfa4ef5a8d0979fc8caa8bb1fc40a2b644
thuanCEO <dohuuthuan.bhdn@gmail.com>
2024-06-17 04:51:18 +0700

    sub products

--- JewelrySalesSystem_NoName_BE/JSS_BusinessObjects/Models/SubProducts.cs
+++ JewelrySalesSystem_NoName_BE/JSS_BusinessObjects/Models/SubProducts.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JSS_BusinessObjects.Models
 {
     public partial class SubProducts
     {
         public Guid Id { get; set; }
-        public string TitleProductName { get; set; }
-        public string Description { get; set; }
-        public int Status { get; set; }
+        public string? TitleProductName { get; set; }
+        public string? Description { get; set; }
+        public int? Status { get; set; }
     }
 }
---
+++ JewelrySalesSystem_NoName_BE/JSS_Services/Implement/SubProductsService.cs
@@ -0,0 +1,26 @@
+﻿using JSS_BusinessObjects.Models;
+using JSS_BusinessObjects.Payload.Response;
+using JSS_BusinessObjects;
+using JSS_DataAccessObjects;
+using JSS_Repositories;
+using JSS_Services.Interface;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSS_Services.Implement
+{
+    public class SubProductsService : BaseService<SubProductsService>, ISubProductsService
+    {
+        public SubProductsService(IUnitOfWork<JewelrySalesSystemContext> unitOfWork, ILogger<SubProductsService> logger) : base(unitOfWork, logger)
+        {
+        }
+        public async Task<IEnumerable<SubProducts>> GetAllSubProductslsAsync()
+        {
+            return await _unitOfWork.GetRepository<SubProducts>().GetListAsync();
+        }
+    }
+}
