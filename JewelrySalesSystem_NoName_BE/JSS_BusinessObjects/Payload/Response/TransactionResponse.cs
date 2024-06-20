43ff3f9dbb8a14934b2bf95ee6906a9e25529a8f
ngandolh <nganttk002@gmail.com>
2024-06-20 07:20:50 +0700

    fix: Transaction create in order - List transaction

---
+++ JewelrySalesSystem_NoName_BE/JSS_BusinessObjects/Payload/Response/TransactionResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSS_BusinessObjects.Payload.Response
+{
+    public class TransactionResponse
+    {
+        public Guid Id { get; set; }
+
+        public Guid OrderId { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Currency { get; set; }
+
+        public double? TotalPrice { get; set; }
+
+        public DateTime? InsDate { get; set; }
+        public OrderResponse Order { get; set; }
+    }
+}
--- JewelrySalesSystem_NoName_FE/DTOs/Transactions/TransactionDTO.cs
+++ JewelrySalesSystem_NoName_FE/DTOs/Transactions/TransactionDTO.cs
@@ -14,4 +14,27 @@
 
         public virtual Order Order { get; set; } = null!;
     }
+
+    public class TransactionOrderDTO
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public string Description { get; set; }
+        public string Currency { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime InsDate { get; set; }
+        public OrderTransactionDTO Order { get; set; }
+    }
+
+    public class OrderTransactionDTO
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public string Type { get; set; }
+        public DateTime InsDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal MaterialProcessPrice { get; set; }
+        public Guid Discount { get; set; }
+        public Guid Promotion { get; set; }
+    }
 }
