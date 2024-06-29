41d5e4bbe44080edf8a4ee21257eaa9b54c7d14c
Miraitowa79 <134464103+Miraitowa79@users.noreply.github.com>
2024-06-30 01:48:55 +0700

    Cancel Old Search Function

--- JewelrySalesSystem_NoName_FE/Pages/Staff/Orders/ListOrder.cshtml.cs
+++ JewelrySalesSystem_NoName_FE/Pages/Staff/Orders/ListOrder.cshtml.cs
@@ -18,12 +18,8 @@ namespace JewelrySalesSystem_NoName_FE.Pages.Staff.Orders
         private readonly IHttpClientFactory _httpClientFactory;
 
         public IList<OrderDTO> OrderList { get; set; } = new List<OrderDTO>();
-        public IList<OrderDTO> FilteredOrderList { get; set; } = new List<OrderDTO>();
         public IList<OrderDetailList> OrderDetailList { get; set; } = new List<OrderDetailList>();
         public OrderForCustomer CustomerOrder { get; set; }
-
-        [BindProperty(SupportsGet = true)]
-        public SearchCriteriaDTO SearchCriteria { get; set; } = new SearchCriteriaDTO();
 
         public ListOrderModel(IHttpClientFactory httpClientFactory)
         {
@@ -33,7 +29,6 @@ namespace JewelrySalesSystem_NoName_FE.Pages.Staff.Orders
         public async Task OnGetAsync()
         {
             await LoadOrderListAsync();
-            ApplyFilters();
         }
 
         private async Task LoadOrderListAsync()
@@ -49,42 +44,6 @@ namespace JewelrySalesSystem_NoName_FE.Pages.Staff.Orders
             else
             {
                 ModelState.AddModelError(string.Empty, "Error retrieving order data. Please try again later.");
-            }
-        }
-
-        private void ApplyFilters()
-        {
-            var filteredOrders = OrderList.AsQueryable();
-
-            if (SearchCriteria.OrderId.HasValue)
-            {
-                filteredOrders = filteredOrders.Where(o => o.Id == SearchCriteria.OrderId.Value);
-            }
-
-            if (SearchCriteria.StartDate.HasValue)
-            {
-                filteredOrders = filteredOrders.Where(o => o.InsDate >= SearchCriteria.StartDate.Value);
-            }
-
-            FilteredOrderList = filteredOrders.ToList();
-        }
-
-        public async Task<IActionResult> OnGetFilterOrderAsync(string phone)
-        {
-            var token = HttpContext.Session.GetString("Token");
-            var client = _httpClientFactory.CreateClient("ApiClient");
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-            var apiUrl = $"{ApiPath.SearchOrderCustomer}?phone={phone}";
-            var response = await client.GetAsync(apiUrl);
-            if (response.IsSuccessStatusCode)
-            {
-                var jsonResponse = await response.Content.ReadAsStringAsync();
-                CustomerOrder = JsonConvert.DeserializeObject<OrderForCustomer>(jsonResponse);
-                return new JsonResult(new { success = true, data = CustomerOrder });
-            }
-            else
-            {
-                return new JsonResult(new { success = false, message = "Khách hàng hiện tại không có đơn hàng đã mua" });
             }
         }
 
@@ -107,9 +66,4 @@ namespace JewelrySalesSystem_NoName_FE.Pages.Staff.Orders
         }
     }
 
-    public class SearchCriteriaDTO
-    {
-        public Guid? OrderId { get; set; }
-        public DateTime? StartDate { get; set; }
-    }
 }
