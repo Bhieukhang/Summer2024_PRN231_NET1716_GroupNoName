6f5a5332fa149998924cb2f4cd6fbc691e203323
baosetsuna123 <baohse321@gmail.com>
2024-07-02 23:37:42 +0700

    adding dashboard chart

--- JewelrySalesSystem_NoName_BE/JSS_BusinessObjects/Payload/Request/DashboardRequest.cs
+++ JewelrySalesSystem_NoName_BE/JSS_BusinessObjects/Payload/Request/DashboardRequest.cs
@@ -3,6 +3,15 @@
     public class DashboardRequest
     {
         public int TotalPromotion { get; set; }
+        
         public decimal TotalRevenue { get; set; }
+
+        public List<MonthlyRevenue> MonthlyRevenue { get; set; } = new();
+    }
+
+    public class MonthlyRevenue
+    {
+        public int Month { get; set; }
+        public double Value { get; set; }
     }
 }
